e58e5a1b83eab08b3d89daf06dfc5bbc89f6804d
sofia durkan <sofiadurkan@gmail.com>
2021-07-11 22:16:37 -0500

    app.js added

---
+++ app.js
@@ -0,0 +1,7 @@
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const element =  document.getElementById("element");
+
+element.insertAdjacentHTML(position,text)
